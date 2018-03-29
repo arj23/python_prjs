fbf53e71c79c2068de1ea8e777fbfef8b7b48284
Ahmadreza Jahani <ahmadreza.jahani@gmail.com>
2018-03-30 01:05:13 +0430

    mix calculate_sw and create_sample

--- data_prjs/stocktwits_datamining/classification/calculate_sw.py
+++ data_prjs/stocktwits_datamining/classification/calculate_sw.py
@@ -1,6 +1,7 @@
 import pymongo
 import urllib
 import csv
+import random
 username = 'admin'
 password = urllib.parse.quote_plus('abc!@#QWE')
 
@@ -27,8 +28,19 @@ def read_lexicon(csv_path) :
 
 def main() :
     db_obj = connect_to_mongodb(db_address, 'stocktwits')
+    bullish_collection  = db_obj['bullish_msg']
     bullish_collection_sample  = db_obj['bullish_msg_sample']
+    bearish_collection = db_obj['bearish_msg']
     bearish_collection_sample  = db_obj['bearish_msg_sample']
+    for i in range(75000) :
+        count = bullish_collection.count()
+        result = bullish_collection.find()[random.randrange(count)]
+        bullish_collection_sample.insert(result)
+        bullish_collection.remove(result)
+        count = bearish_collection.count()
+        result = bearish_collection.find()[random.randrange(count)]
+        bearish_collection_sample.insert(result)
+        bearish_collection.remove(result)
     sw_collection  = db_obj['sw_collection']
     lexicon_L1 = read_lexicon("l1_lexicon.csv")
     term_list = []
