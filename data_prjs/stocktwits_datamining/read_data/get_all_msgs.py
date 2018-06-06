76d653e6d26ccf9ad670e69ce693557991357cc1
Ahmadreza Jahani <ahmadreza.jahani@gmail.com>
2018-06-06 16:04:49 +0430

    update get all data with 3 tokens

--- data_prjs/stocktwits_datamining/read_data/get_all_msgs.py
+++ data_prjs/stocktwits_datamining/read_data/get_all_msgs.py
@@ -10,17 +10,19 @@ username = 'admin'
 password = urllib.parse.quote_plus('abc!@#QWE')
 
 db_address = 'mongodb://'+ username +':' + password + '@88.99.153.217/admin?authSource=admin'
-
+token_list = ['94b81109ac49597b357c75c5870526e3894877d8',
+              '7e297ff94f94fc1dd3d8047afc6be52bc0080ace',
+              '75c44c9341cf26a5383dfd76b687ee30817dd601']
 def connect_to_mongodb (db_url, db_name):
     connection = pymongo.MongoClient(db_url)
     return connection[db_name]
 
-def get_messages_from_stocktwits(last_message_id=None):
+def get_messages_from_stocktwits(token,last_message_id=None):
     api_url = ''
     if last_message_id is None:
-        api_url = 'https://api.stocktwits.com/api/2/streams/all.json?access_token=75c44c9341cf26a5383dfd76b687ee30817dd601&filter=top&max'
+        api_url = 'https://api.stocktwits.com/api/2/streams/all.json?access_token={}&filter=top&max'.format(token)
     else:
-        api_url = 'https://api.stocktwits.com/api/2/streams/all.json?access_token=75c44c9341cf26a5383dfd76b687ee30817dd601&filter=top&max={}'.format(last_message_id)
+        api_url = 'https://api.stocktwits.com/api/2/streams/all.json?access_token={}&filter=top&max={}'.format(token,last_message_id)
     req = requests.get(api_url)
     data = json.loads(req.text)
     return data.get('messages')
@@ -68,24 +70,29 @@ def main() :
     bullish_collection = db_obj['bullish_collection']
     bearish_collection = db_obj['bearish_collection']
 
-    max_count = 90
+    max_count = 1000000
     print(max_count)
     message_count = 0
     low_id = 0;
+    token_list_id =0;
     while (message_count < max_count):
         if low_id is 0:
             print("there is no message in db to get lowest id")
             result = msgs_collection.find().sort("id", pymongo.ASCENDING).limit(1);
             for r in result:
                 low_id = r['id']
-        messages = get_messages_from_stocktwits(low_id)
+        messages = get_messages_from_stocktwits(token_list[token_list_id], low_id)
         if messages is None or len(messages) == 0:
             print('Receive no message from request, trying again...')
+            token_list_id += 1
+            if token_list_id == len(token_list) :
+                token_list_id = 0
+            time.sleep(10)
             continue
         for message in messages:
             date_str = message['created_at'];
             datetime_obj = str_datetime_to_obj_datetime(date_str)
-            print(datetime_obj)
+            # print(datetime_obj)
             clean_message = message_cleaning(message['body'])
             low_id = message['id']
             if (message['entities']['sentiment']):
@@ -129,8 +136,8 @@ def main() :
 
 
             message_count += 1;
-        print("{} Messages added to database...".format(message_count));
-        time.sleep(30)
+        # print("{} Messages added to database...".format(message_count));
+        time.sleep(3)
     print("Threshold of messages satisfied")
 
 if __name__ == "__main__": main()
